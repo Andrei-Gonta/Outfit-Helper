336c6a15e618c19e99ca898d01826d77d7e8a8f1
Andrei Gonta <gontaandrei02@gmail.com>
2025-01-14 23:29:15 +0200

    result activity (beta 2.0)

--- app/src/main/java/com/example/labproject/activities/ResultActivity.kt
+++ app/src/main/java/com/example/labproject/activities/ResultActivity.kt
@@ -1,7 +1,9 @@
 package com.example.labproject.activities
 
+import android.annotation.SuppressLint
 import android.health.connect.datatypes.units.Temperature
 import android.os.Bundle
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
@@ -11,6 +13,7 @@ import androidx.core.view.WindowInsetsCompat
 import com.example.labproject.R
 
 class ResultActivity : AppCompatActivity() {
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -20,17 +23,34 @@ class ResultActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
-        val resultTextView = findViewById<TextView>(R.id.resultTextView)
+        val  message_top= findViewById<TextView>(R.id.message_top)
+        val  message_bottom= findViewById<TextView>(R.id.message_bottom)
+        val  message_bottom2= findViewById<TextView>(R.id.message_bottom2)
+
 
         val temp_value = intent.getIntExtra("Temprature", 0)
 
+        val hat: ImageView = findViewById(R.id.hat)
+        val jacket: ImageView = findViewById(R.id.jacket)
+        val jeans: ImageView = findViewById(R.id.jeans)
+        val boots: ImageView = findViewById(R.id.boots)
+
+
+
         if(temp_value < 10)
         {
-            resultTextView.text = "Hot"
+            hat.setImageResource(R.drawable.hat)
+            jacket.setImageResource(R.drawable.blue_jacket)
+            jeans.setImageResource(R.drawable.pants2)
+            boots.setImageResource(R.drawable.boots)
+            message_top.text = "This is your outfit!"
+            message_bottom.text = "There is a high chance of precipitation, don't forget to take an umbrella with you!"
+            message_bottom2.text = "Have a wonderful day!"
+
         }
         else
         {
-            resultTextView.text = "Cold"
+            message_top.text = "Another outfit"
         }
 
 
